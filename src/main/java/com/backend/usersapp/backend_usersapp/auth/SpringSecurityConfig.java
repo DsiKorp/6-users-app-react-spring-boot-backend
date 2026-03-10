a2d10f6d365fc9cd9d531b59643f50d94bb7113a
Juan Delgado <dsikorp@gmail.com>
2026-03-10 13:31:46 -0500

    AllowedOriginPatterns ready

--- src/main/java/com/backend/usersapp/backend_usersapp/auth/SpringSecurityConfig.java
+++ src/main/java/com/backend/usersapp/backend_usersapp/auth/SpringSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.backend.usersapp.backend_usersapp.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -13,13 +16,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.core.Ordered;
-
-import java.util.Arrays;
 
 import com.backend.usersapp.backend_usersapp.auth.filters.JwtAuthenticationFilter;
 import com.backend.usersapp.backend_usersapp.auth.filters.JwtValidationFilter;
@@ -68,6 +68,7 @@ public class SpringSecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
