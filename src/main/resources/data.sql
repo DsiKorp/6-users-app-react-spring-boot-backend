afd2a9fde59d4f75630d17d699fb54145249d23d
Juan Delgado <dsikorp@gmail.com>
2026-03-10 19:54:34 -0500

    AWS Aurora RDS My SQL prod working

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,8 @@
 INSERT IGNORE INTO users (username, email, password) VALUES
-('admin', 'admin@example.com', 'admin123'),
+('admin', 'admin@example.com', 'paawdmin123'),
 ('juan', 'juan@example.com', 'cascade123'),
-('maria', 'maria@example.com', 'maria123'),
-('pedro', 'pedro@example.com', 'pedro123'),
-('sofia', 'sofia@example.com', 'sofia123'),
-('lucas', 'lucas@example.com', 'lucas123'),
-('valentina', 'valentina@example.com', 'valentina123'),
-('andres', 'andres@example.com', 'andres123'),
-('camila', 'camila@example.com', 'camila123'),
 ('laura', 'laura@example.com', 'laura123');
+
+-- INSERT IGNORE INTO roles (name) VALUES
+-- ('ROLE_ADMIN'),
+-- ('ROLE_USER');
